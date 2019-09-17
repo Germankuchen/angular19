fb8de260ab4f8a58b6fdac4e1944b8405696a305
GermanKuchen <germankuchen@gmail.com>
2019-09-17 08:53:03 -0300

    lo ultimo

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { map, filter } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -11,12 +12,14 @@ export class BreadcrumbsComponent implements OnInit {
 
   titulo: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private title: Title) {
     this.router.events.pipe(filter(info => info instanceof ActivationEnd), 
                             map((info: ActivationEnd) => info.snapshot.data)).subscribe(info => {
                               console.log(info);
                               if (info.titulo !== undefined) {
                                 this.titulo = info.titulo;
+                                this.title.setTitle('Framework Angular');
                               }
                             });
 
